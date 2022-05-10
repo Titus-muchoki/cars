76b35db3a30de4716219ccced57e9b8c65cbb817
kajela <titoyut56@gmail.com>
2022-05-10 06:57:50 -0400

    Initial commit

---
+++ src/cars.java
@@ -0,0 +1,11 @@
+public class cars {
+        int y =4;
+        int x =6;
+        public static void main(String[] args) {
+            cars myObj = new cars();
+            cars myObj1 = new cars();
+            System.out.println(myObj.y);
+            System.out.println(myObj1.x);
+
+        }
+    }
